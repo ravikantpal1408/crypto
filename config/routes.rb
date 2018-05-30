780fa98290c9c3f4300414fc51f89057ce80e503
Ravi Kant Pal <ravikantpal123@gmail.com>
2018-05-30 13:58:52 +0000

    just like that

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   #post 'webservices/getCity'
   get 'webservices/getMyLoc'
   get 'myblogs' , to: 'home#blogspage'
-  post 'myBlog',to: 'webservices#getmyblog'
+  post 'myBlog', to: 'webservices#getmyblog'
   #get 'testblog',to: 'webservices#getmyblog'
   get 'webservices/checkRegisteredVehicle'
 end
--- public/blogs/myblogs.js
+++ public/blogs/myblogs.js
@@ -17,6 +17,7 @@ jQuery(document).ready(function(){
 
 function getMyBlog(id)
 {
+    console.log(id)
     $.ajax({
         type: 'POST',
         url: '/myBlog',
